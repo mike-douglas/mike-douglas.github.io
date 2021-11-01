bf3cbe0d4544b6e82322a0b27318a6955823a39e
Mike Douglas <michael.j.douglas@gmail.com>
2021-11-01 09:27:45 -0400

    New website release

---
+++ docs/_nuxt/static/1635773246/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635773246",layout:"hero",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2d36d142"}}}}(null,"\u002F"));
