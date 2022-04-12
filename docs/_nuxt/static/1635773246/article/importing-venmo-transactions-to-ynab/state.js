c791de443578fe70d067123d3e0135927b496237
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2022-04-11 22:55:01 -0400

    New site

--- docs/_nuxt/static/1635773246/article/importing-venmo-transactions-to-ynab/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635773246",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fimporting-venmo-transactions-to-ynab",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2d36d142"}}}}(null));
---
+++ docs/_nuxt/static/1649732021/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649732021",layout:"hero",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad1f82bb"}}}}(null,"\u002F"));
